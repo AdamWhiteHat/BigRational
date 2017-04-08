a1e0babcb90aa4e3ab68ccfa00633a8fab7db060
AdamRakaska <arakaska@gmail.com>
2017-04-08 02:21:33 -0600

    TestFraction Completed

--- TestBigRational/TestFraction.cs
+++ TestBigRational/TestFraction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ExtendedNumerics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestBigRational
@@ -9,31 +10,71 @@ namespace TestBigRational
 		[TestMethod]
 		public void TestAddition()
 		{
+			Fraction oneThird = new Fraction(1, 3);
+			Fraction oneFifth = new Fraction(1, 5);
+
+			Fraction expectedValue = new Fraction(8, 15);
+			
+			Fraction result = Fraction.Add(oneThird, oneFifth);
+			Assert.AreEqual(expectedValue, result);
 		}
 
 		[TestMethod]
 		public void TestSubtraction()
 		{
+			Fraction oneHalf = new Fraction(1, 2);
+			Fraction oneSixth = new Fraction(1, 6);
+
+			Fraction expectedValue = new Fraction(1, 3);
+
+			Fraction result = Fraction.Subtract(oneHalf, oneSixth);
+			Assert.AreEqual(expectedValue, result);
 		}
 
 		[TestMethod]
 		public void TestMultiplication()
 		{
+			Fraction oneHalf = new Fraction(1, 2);
+			Fraction twoFifths = new Fraction(2, 5);
+
+			Fraction expectedValue = new Fraction(1, 5);
+
+			Fraction result = Fraction.Multiply(oneHalf, twoFifths);
+			Assert.AreEqual(expectedValue, result);
 		}
 
 		[TestMethod]
 		public void TestDivision()
 		{
+			Fraction oneHalf = new Fraction(1, 2);
+			Fraction oneSixth = new Fraction(1, 6);
+
+			Fraction expectedValue = new Fraction(3, 1);
+
+			Fraction result = Fraction.Divide(oneHalf, oneSixth);
+			Assert.AreEqual(expectedValue, result);
 		}
 
 		[TestMethod]
 		public void TestConvertToDouble()
 		{
+			Fraction oneSixteenth = new Fraction(1, 16);
+			Double expectedValue = 0.0625d;
+
+			Double result = (Double)oneSixteenth;
+
+			Assert.AreEqual(expectedValue, result);
 		}
 
 		[TestMethod]
 		public void TestConvertFromDouble()
 		{
+			Double fifteenSixteenths = 0.9375d;
+			Fraction expectedValue = new Fraction(15, 16);
+			
+			Fraction result = (Fraction)fifteenSixteenths;
+
+			Assert.AreEqual(expectedValue, result);
 		}
 	}
 }
