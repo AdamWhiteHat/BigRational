368e56fb42fe7e32411d7de5b08459cb1ce0fef4
AdamRakaska <arakaska@gmail.com>
2017-04-07 18:31:58 -0600

    Initial code check-in

---
+++ BigRational/BigRational.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ExtendedNumerics
+{
+	public class BigRational
+	{
+		public BigInteger WholePart { get; private set; }
+		public Fraction FractionalPart { get; private set; }
+
+		public BigRational(BigInteger value)
+		{
+			WholePart = value;
+		}
+
+		public BigRational(BigInteger whole, Fraction fraction)
+			: this(whole)
+		{
+			FractionalPart = fraction;
+		}
+
+
+	}
+}
---
+++ BigRational/Fraction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ExtendedNumerics
+{
+	public class Fraction
+	{
+		public BigInteger Numerator { get; private set; }
+		public BigInteger Denominator { get; private set; }
+
+		public Fraction(BigInteger value)
+		{
+			Numerator = value;
+			Denominator = BigInteger.One;
+		}
+
+		public Fraction(BigInteger numerator, BigInteger denominator)
+		{
+			Numerator = numerator;
+			Denominator = denominator;
+		}
+	}
+}
