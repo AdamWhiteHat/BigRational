0985f87d20135d4af2c695c8179ca6da2136c8dc
AdamRakaska <arakaska@gmail.com>
2017-04-08 01:46:19 -0600

    Test Project initial check-in

---
+++ TestBigRational/TestFraction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestBigRational
+{
+	[TestClass]
+	public class TestFraction
+	{
+		[TestMethod]
+		public void TestAddition()
+		{
+		}
+
+		[TestMethod]
+		public void TestSubtraction()
+		{
+		}
+
+		[TestMethod]
+		public void TestMultiplication()
+		{
+		}
+
+		[TestMethod]
+		public void TestDivision()
+		{
+		}
+
+		[TestMethod]
+		public void TestConvertToDouble()
+		{
+		}
+
+		[TestMethod]
+		public void TestConvertFromDouble()
+		{
+		}
+	}
+}
